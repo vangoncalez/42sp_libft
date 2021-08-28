1b1cf0f45c5d1f7c02f512f90c8cbad02293f758
Vanessa Gonçalez <vanessagoncalez@hotmail.com>
2021-08-28 16:40:30 -0300

    ft_atoi alterada com int long

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vaferrei <vaferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 14:23:11 by vaferrei          #+#    #+#             */
-/*   Updated: 2021/08/27 08:52:45 by vaferrei         ###   ########.fr       */
+/*   Updated: 2021/08/28 16:39:29 by vaferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ int	ft_isspace(char c)
 	return (0);
 }
 
-int	ft_atoi(char *str)
+int	ft_atoi(const char *str)
 {
-	int	neg;
-	int	i;
-	int	num;
+	long int	neg;
+	long int	i;
+	long int	num;
 
 	i = 0;
 	neg = 1;
@@ -38,7 +38,7 @@ int	ft_atoi(char *str)
 			neg *= -1;
 		i++;
 	}
-	while (str[i] >= 48 && str[i] <= 57)
+	while (ft_isdigit(str[i]))
 	{
 		num = num * 10 + (str[i] - 48);
 		i++;
